3b772c1d94032e4f035d70cad5de88e97e3ecfe7
Anders Eliasson <anders.eliasson@digitalroute.com>
2016-02-24 13:40:05 +0100

    stuff

--- src/loc/core.clj
+++ src/loc/core.clj
@@ -1,4 +1,5 @@
 (ns loc.core
+  (:use [clojure.pprint])
   (:import [java.io File]))
 
 
@@ -26,3 +27,14 @@
   (_loc filter-fn (File. f-name)))  
   ([f-name]
     (loc java-filter f-name)))
+
+(defn package-loc 
+  ([package-dir sub-dir]
+  (apply hash-map (mapcat (fn [f] [(keyword (.getName f)), {(keyword sub-dir) (_loc #(or (java-filter %) (scala-filter %)) (File. f sub-dir))}]) (filter #(.isDirectory %) (.listFiles (File. package-dir))))))
+  ([package-dir]
+    (merge-with 
+      merge
+      (package-loc package-dir "src")
+      (package-loc package-dir "test")
+      (package-loc package-dir "build")
+   )))
