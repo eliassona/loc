0532830c9edbc3afef4afe4bd4f247dbb1330155
Anders Eliasson <anders.eliasson@digitalroute.com>
2016-02-26 11:21:39 +0100

    use 1.8

--- src/loc/core.clj
+++ src/loc/core.clj
@@ -1,12 +1,7 @@
 (ns loc.core
   (:use [clojure.pprint])
+  (:require [apartial.core :refer [apartial]])
   (:import [java.io File]))
-
-
-(defmacro dbg [body]
-  `(let [x# ~body]
-     (println "dbg:" '~body "=" x#)
-     x#))
 
 (defn java-filter [f] (.endsWith (.getName f) ".java"))
 (defn scala-filter [f] (.endsWith (.getName f) ".scala"))
@@ -14,23 +9,27 @@
 
 
 (defn count-loc [f] (count (.split (slurp f) "\n")))
+(defprotocol ILoc
+  (loc [this filter-fn]))
 
-(defn _loc [filter-fn f]
-  (if (.isFile f)
+(extend-protocol ILoc
+  File
+  (loc [f filter-fn]
+    (if (.isFile f)
     (if (filter-fn f)
       (count-loc f)
       0)
-    (reduce + (map (partial _loc filter-fn) (.listFiles f)))))
+    (reduce + (map (apartial loc _ filter-fn) (.listFiles f)))))
+  String 
+  (loc [filename filter-fn] (loc (File. filename) filter-fn))
+  java.util.List
+  (loc [l filter-fn] (map (apartial loc _ filter-fn) l)) 
+  )
   
-(defn loc 
-  ([filter-fn f-name]
-  (_loc filter-fn (File. f-name)))  
-  ([f-name]
-    (loc java-filter f-name)))
 
 (defn package-loc 
   ([package-dir sub-dir]
-  (apply hash-map (mapcat (fn [f] [(keyword (.getName f)), {(keyword sub-dir) (_loc #(or (java-filter %) (scala-filter %)) (File. f sub-dir))}]) (filter #(.isDirectory %) (.listFiles (File. package-dir))))))
+  (apply hash-map (mapcat (fn [f] [(keyword (.getName f)), {(keyword sub-dir) (loc (File. f sub-dir) #(or (java-filter %) (scala-filter %)))}]) (filter #(.isDirectory %) (.listFiles (File. package-dir))))))
   ([package-dir]
     (merge-with 
       merge
